17ec2401f5de06e2b6d22d0b98dfbfd331af967b
AkifNaveed12 <akif27031@gmail.com>
2026-06-23 23:27:36 +0500

    day 3 math library practice

--- day_3/scripts3.js
+++ day_3/scripts3.js
@@ -9,13 +9,36 @@
 // console.log(random);
 
 const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 
 const min = 1;
 const max = 6;
 
-let randomNum;
-myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min;
-    myLabel.textContent = randomNum;
+let randomNum1, randomNum2, randomNum3;
+myButton.onclick = function () {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+};
+
+
+//if statements
+let age = 25;
+if(age>=18){
+    console.log("you are old enough to drive!");
+}else{
+    console.log("you are not old enough to drive!");
+}
+
+let isStudent = true;
+if(isStudent){
+    console.log("you are a student!");
+}
+else{
+    console.log("you are not a student!");
 }
