9da3ea7e52f7c8926e07ae53f4c322236729510c
AkifNaveed12 <akif27031@gmail.com>
2026-07-07 12:50:36 +0500

    promises method 2

--- chaiaurcodeplaylistlectures/promises.js
+++ chaiaurcodeplaylistlectures/promises.js
@@ -1,4 +1,4 @@
-//lets make promise
+//lets make promise no 1
 const promiseOne = new Promise(function(resolve, reject){
     //any Async Function i.e DB Calls, cryptography, network requests
 
@@ -11,4 +11,17 @@ const promiseOne = new Promise(function(resolve, reject){
 
 promiseOne.then(function(){ // promise consumption,  auto argument recieving, value is returned
     console.log(`promise consumption!`)
+})
+
+
+
+//promise 2
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log(`promise async 2 is executed!`)
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log(`async 2 then executed`)
 })
